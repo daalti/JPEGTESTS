73a345d9a587bed018bd4415696dcd4a3e6f3ca5
daalti <114062134+daalti@users.noreply.github.com>
2025-07-23 15:03:13 +0200

    Add new PWG tests in new framework

---
+++ pwg_new/test_when_printing_pwg_cloud_print_print_quality_3.py
@@ -0,0 +1,79 @@
+import logging
+from dunetuf.print.print_common_types import MediaInputIds, MediaSize, MediaType
+from dunetuf.print.new.output.output_saver import OutputSaver
+from tests.print.pdl.print_base import TestWhenPrinting, setup_output_saver, tear_down_output_saver
+
+
+class TestWhenPrintingJPEGFile(TestWhenPrinting):
+    @classmethod
+    def setup_class(cls):
+        """Initialize shared test resources."""
+        super().setup_class()
+        cls.outputsaver = OutputSaver()
+        setup_output_saver(cls.outputsaver)
+
+    @classmethod
+    def teardown_class(cls):
+        """Release shared test resources."""
+
+    def teardown_method(self):
+        """Clean up resources after each test."""
+        # Clear job queue
+        self.job_queue.cancel_all_jobs()
+        self.job_queue.wait_for_queue_empty()
+
+        # Clear job history
+        self.job_history.clear()
+        self.job_history.wait_for_history_empty()
+
+        # Reset media configuration to default
+        self.media.update_media_configuration(self.default_configuration)
+        tear_down_output_saver(self.outputsaver)
+    """
+$$$$_BEGIN_TEST_METADATA_DECLARATION_$$$$$
+    +purpose:Simple print job of pwg cloud print-print quality-3 from *PwgCloudPrint-PrintQuality-3.pwg file
+    +test_tier:1
+    +is_manual:False
+    +test_classification:System
+    +reqid:DUNE-12138
+    +timeout:120
+    +asset:PDL_New
+    +delivery_team:QualityGuild
+    +feature_team:PDLSolns
+    +test_framework:TUF
+    +external_files:PwgCloudPrint-PrintQuality-3.pwg=23786946e342d1612d21821f5dfbd747b8be869bbe9b7f053e563c19801f4eb0
+    +name:TestWhenPrintingJPEGFile::test_when_using_pwg_cloud_print_print_quality_3_file_then_succeeds
+    +categorization:
+        +segment:Platform
+        +area:Print
+        +feature:PDL
+        +sub_feature:PWGRaster
+        +interaction:Headless
+        +test_type:Positive
+    +test:
+        +title:test_pwg_cloud_print_print_quality_3
+        +guid:72e21c4b-d832-4232-9bdd-a7f791d0bf26
+        +dut:
+            +type:Simulator
+            +configuration:DocumentFormat=PWGRaster
+
+    +overrides:
+        +Enterprise:
+            +is_manual:False
+            +timeout:600
+            +test:
+                +dut:
+                    +type:Emulator
+
+
+$$$$$_END_TEST_METADATA_DECLARATION_$$$$
+    """
+    def test_when_using_pwg_cloud_print_print_quality_3_file_then_succeeds(self):
+
+        job_id = self.print.raw.start('23786946e342d1612d21821f5dfbd747b8be869bbe9b7f053e563c19801f4eb0')
+        self.print.wait_for_job_completion(job_id)
+        self.outputsaver.save_output()
+        logging.info("Get crc value for the current print job")
+        Current_crc_value = self.outputsaver.get_crc()
+        logging.info("Validate current crc with master crc")
+        assert self.outputsaver.verify_pdl_crc(Current_crc_value), "fail on crc mismatch"
