0df16d591fb5cdb1ba8a0224001d1a2ff30d6090
Daniel Alonso Tirapu <daaltigo@gmail.com>
2025-09-04 12:30:49 +0200

    Delete all

--- new/shared/protocols/copy_logic.py
+++
@@ -1,6 +0,0 @@
-# shared/protocols/copy_logic.py
-from typing import Protocol
-from .copy_ops import CopyOps
-
-class CopyLogic(Protocol):
-    def execute(self, ops: CopyOps, state, ui) -> str: ...
